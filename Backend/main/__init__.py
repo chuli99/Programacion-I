f52823e0282fa1230f767fd76aa5913e79d87c34
Julian <juliancastillo0399@gmail.com>
2022-03-30 10:40:43 -0300

    Añado codigo a __init__.py el cual carga a la API todos los recursos que utilizaremos

--- Backend/main/__init__.py
+++ Backend/main/__init__.py
@@ -1,12 +1,31 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
+from flask_restful import Api
+
+#Directorio de recursos
+import main.resources as resources
+
+api = Api()
 
 def create_app():
 
 	app = Flask(__name__)
-
 	load_dotenv()
+	#Cargamos a la API Poems
+	api.add_resource(resources.PoemsResource,'/poems')
+	#Cargamos a la API Poem
+	api.add_resource(resources.PoemResource,'/poem/<id>')
+	#Cargamos a la API Users
+	api.add_resource(resources.UsersResource,'/users')
+	#Cargamos a la API User
+	api.add_resource(resources.UserResource,'/user/<id>')
+	#Cargamos a la API Feedbacks
+	api.add_resource(resources.FeedbacksResource,'/feedback')
+	#Cargamos a la API Feedback
+	api.add_resource(resources.FeedbackResource,'/feedback/<id>')
 
+	#Cargamos la aplicacion en la API de Flask Restful
+	api.init_app(app)
 	return app
 
