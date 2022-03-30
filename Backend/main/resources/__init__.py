53bb9d5aad722a417044db7feb55bb696a9a2e2c
Julian <juliancastillo0399@gmail.com>
2022-03-30 11:03:34 -0300

    Creo __init__.py en resources

---
+++ Backend/main/resources/__init__.py
@@ -0,0 +1,7 @@
+from .Poem import Poem as PoemResource
+from .Poem import Poems as PoemsResource
+from .User import User as UserResource
+from .User import Users as UsersResource
+from .Feedback import Feedback as FeedbackResource
+from .Feedback import Feedbacks as FeedbacksResource
+
