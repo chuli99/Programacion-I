82947ab2ebf2da6b725787008920088ccaf3e38b
Julian <juliancastillo0399@gmail.com>
2022-04-05 16:56:52 -0300

    Inicializo la data base

--- Backend/app.py
+++ Backend/app.py
@@ -1,11 +1,12 @@
 from main import create_app
 import os
-
+from main import db 
 
 app = create_app()
 
 app.app_context().push()
 
 if __name__ == '__main__':
+	db.create_all()
 	app.run(port = os.getenv("PORT"),debug = True)
 
