bad29f46500234c9079182d375d0b2fb5293b7f6
Julian <juliancastillo0399@gmail.com>
2022-04-07 18:35:32 -0300

    Anado tabla de User.py para interactuar con la base de datos

--- Backend/main/models/User.py
+++ Backend/main/models/User.py
@@ -1,10 +1,28 @@
+import email
 import sqlite3
+from unicodedata import name
 from models import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_Id = db.Column(db.Integer, nullable= False)
-    poem_Id = db.columb(db.Integer, nullable= False)
-    qualifications = db.columb(db.Integer, nullable = False)
-    comment = db.column(db.string(100), nullable = False)
+    name = db.column(db.string(100), nullable = False)
+    email = db.column(db.string(100), nullable = False)
+    password = db.column(db.string(100), nullable = False)
+
+    def to_json(self):
+        json_string = {
+            'id': self.id, 
+            'name': self.name, 
+            'email': self.email
+        }
+        return (json_string)
     
+    
+    @staticmethod
+    def from_json(json_string):
+        id = json_string.get('id')
+        name = json_string.get('name')
+        email = json_string.get('email')
+        password = json_string.get('password')
+        return (User(id = id, name = name, email = email, password = password))
+        
