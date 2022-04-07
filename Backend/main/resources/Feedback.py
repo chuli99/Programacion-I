37fc72d796a3408f448c96e3af2bebe4b2b8c5d3
Julian <juliancastillo0399@gmail.com>
2022-04-07 18:10:02 -0300

    Agrego lo necesario a Feedback para interactuar con las tablas y modelos

--- Backend/main/resources/Feedback.py
+++ Backend/main/resources/Feedback.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
-from flask import request
-
+from flask import jsonify, request
+from .. import db
+from main.models import FeedbackModel
 #Diccionario de prueba
 FEEDBACKS = {
     1: {'comment': 'Buen Poema', 'Autor': 'mauri23'},
@@ -11,31 +12,34 @@ FEEDBACKS = {
 class Feedback(Resource):
     #Obtener recurso
     def get(self, id):
-        #Verificar que exista Feedback con ese Id en diccionario
-        if int(id) in FEEDBACKS:
-            #Devolver feedback correspondiente
-            return FEEDBACKS[int(id)]
-        #Devolver error 404 en caso que no exista
-        return '', 404
+        feedback = db.session.query(FeedbackModel).get_or_404(id)
+        return feedback
     #Eliminar recurso
     def delete(self, id):
-        #Verificar que exista un Poema con ese Id en diccionario
-        if int(id) in FEEDBACKS:
-            #Eliminar feedback del diccionario
-            del FEEDBACKS[int(id)]
-            return '', 204
-        return '', 404
-    
+        feedback = db.session.query(FeedbackModel).get_or_404(id)
+        db.session.delete(feedback)
+        db.session.commit()
+        return '', 204
 
 #Recurso Feedbacks
 class Feedbacks(Resource):
-    #Obtener lista de recursos
     def get(self):
-        return FEEDBACKS
+        feedbacks = db.session.query(FeedbackModel).all()
+        return jsonify([feedback.to_json_short() for feedback in feedbacks])
+
+    """
+            list_feedback = []
+            for feedback in feedbacks:
+                list_feedback.append(feedback.to_json())
+            return jsonify(list_poem)
+    """
+
+
     #Insertar recurso
     def post(self):
         #Obtener datos de la solicitud
-        feedback = request.get_json()
-        id = int(max(FEEDBACKS.keys())) + 1
-        FEEDBACKS[id] = feedback
-        return FEEDBACKS[id], 201
+        feedback = FeedbackModel.from_json(request.get_json())
+        db.session.add(feedback)
+        db.session.commit()
+        return feedback.to_json(), 201
+        
