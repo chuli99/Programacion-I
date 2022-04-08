2a0c0da812fa3248772d67de3a7682a25e7be63d
frames2424 <frames2424@gmail.com>
2022-04-08 10:41:57 -0300

    feeback

--- Backend/main/models/Feedback.py
+++ Backend/main/models/Feedback.py
@@ -1,2 +1,31 @@
+import email
 import sqlite3
+from unicodedata import name
 from models import db
+
+class Feedback(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_Id = db.column(db.string(100), nullable = False)
+    poem_Id = db.column(db.string(100), nullable = False)
+    qualifications = db.column(db.string(100), nullable = False)
+    comment = db.column(db.string(100), nullable = False)
+
+
+
+    def to_json(self):
+        json_string = {
+            'id': self.id, 
+            'poem_Id': self.poem_Id, 
+            'comment': self.comment,
+            'qualifications':self.qualifications
+        }
+        return (json_string)
+    
+    
+    @staticmethod
+    def from_json(json_string):
+        id = json_string.get('id')
+        poem_Id = json_string.get('poem_Id')
+        comment = json_string.get('comment')
+        qualifications = json_string.get('qualifications')
+        return (Feedback(id = id, poem_Id = poem_Id, comment = comment, qualifications = qualifications))
