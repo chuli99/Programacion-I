ab4aa8b6e0fbe39cb293fb988200e5703525ab70
Julian <juliancastillo0399@gmail.com>
2022-04-08 22:53:06 -0300

    Modifico Poem de models para el correcto funcionamiento del programa

--- Backend/main/models/Poem.py
+++ Backend/main/models/Poem.py
@@ -6,21 +6,32 @@ class Poem(db.Model):
     content = db.Column(db.String(500), nullable = False)
     userId = db.Column(db.Integer,db.ForeignKey('user.id'), nullable = False)
 
+    def __repr__(self):
+        return '<Poem: %r %r >' % (self.title, self.content, self.userId)
+
     def to_json(self):
-        json_string = {
+        poem_string = {
             'id': self.id, 
-            'title': self.title, 
-            'content': self.content,
-            'userId': self.userId
+            'title': str(self.title), 
+            'content': str(self.content),
+            'userId': self.userId,
         }
-        return (json_string)
-    
-    
+        return (poem_string)
+    def to_json_short(self):
+        poem_json = {
+            'id': self.id,
+            'title': str(self.title),
+            'content': str(self.content),
+            'userId': self.userId,
+
+        }
+        return(poem_json)
+
     @staticmethod
     def from_json(json_string):
         id = json_string.get('id')
         title = json_string.get('title')
         content = json_string.get('content')
-        userId = json_string.get('UserId')
+        userId = json_string.get('userId')
         return (Poem(id = id, title = title, content = content, userId = userId))
         
