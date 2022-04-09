ef48c7d02c916273227c3485fb10ef8fb6bae3b6
Julian <juliancastillo0399@gmail.com>
2022-04-08 22:53:28 -0300

    Modifico User de models para el correcto funcionamiento del programa

--- Backend/main/models/User.py
+++ Backend/main/models/User.py
@@ -5,16 +5,28 @@ class User(db.Model):
     name = db.Column(db.String(100), nullable = False)
     email = db.Column(db.String(100), nullable = False)
     password = db.Column(db.String(100), nullable = False)
+    
+    def __repr__(self):
+        return '<User: %r %r >' % (self.name, self.email, self.password)
+
 
     def to_json(self):
-        json_string = {
+        user_json = {
             'id': self.id, 
-            'name': self.name, 
-            'email': self.email
+            'name': str(self.name), 
+            'email': str(self.email),
+            'password': str(self.password),
         }
-        return (json_string)
+        return (user_json)
     
-    
+    def to_json_short(self):
+        user_json = {
+            'id': self.id,
+            'name': str(self.name),
+            'email': str(self.email),
+        }
+        return user_json
+
     @staticmethod
     def from_json(json_string):
         id = json_string.get('id')
